fd41d3ec9533c469d3b2c73a27fed3ac90106195
SalmanRajpuat <97193516+SalmanRajpuat@users.noreply.github.com>
2024-03-25 14:23:03 +0500

    js done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// focus the cursor on the email-address input
+// focused the cursor on the email-address input
 const emailField = document.getElementById("email-address-input");
 emailField.focus({
   preventScroll: true,
